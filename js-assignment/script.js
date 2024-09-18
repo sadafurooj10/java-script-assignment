1379a3683fd20b9d6e37f2dbd49ac50bfe3a7347
sadafurooj10 <sadafurooj00@gmail.com>
2024-09-19 00:41:27 +0500

    Add files via upload

---
+++ js-assignment/script.js
@@ -0,0 +1,34 @@
+// QUESTION NO :1
+
+alert ("My Name Is Sadaf Urooj");
+
+// QUESTION NO :2
+
+// Storing the name in a variable
+var name = "Sadaf Urooj";
+
+// Showing name in alert with concatenation
+alert("Hello, My Name Is " + name + "! & This Is My Java Script Assignment.");
+console.log("Hello, My Name Is " + name + "! & This Is My Java Script Assignment.");
+
+// QUESTION NO :3
+
+var a=4;
+var b=2;
+var result= a++ + --b  - --a + b++ + a + b;
+console.log( "The Result Of This Equation Is : " + result);
+
+// QUESTION NO :4
+
+let day = prompt("What's The Day Today?");
+
+if (day === "Saturday" || day === "Sunday") {
+    alert("It's the weekend!");
+} else if (day === "Monday") {
+    alert("It's the start of the work week.");
+} else if (day === "Friday") {
+    alert("It's almost the weekend!");
+} else {
+    alert("It's a regular weekday.");
+}
+
